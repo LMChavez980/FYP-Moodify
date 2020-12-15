2d938be9498fc4e8a9ca1f99bc4a425f1062bc9a
Louis Chavez <rasengan650@yahoo.com.ph>
2020-12-15 03:10:16 +0000

    Prototype almost

--- InterimDemo/app/src/main/java/com/fypmood/interim/MainActivity.java
+++ InterimDemo/app/src/main/java/com/fypmood/interim/MainActivity.java
@@ -1,10 +1,15 @@
 package com.fypmood.interim;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.fypmood.interim.models.Playlist;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -12,21 +17,57 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CLIENT_ID = "4110566732ad4c08b6f0e6c5768e552d";
     private static final String REDIRECT_URI = "com.fypmood.interim://callback";
+    private static final int REQUEST_CODE = 1337;
     private SpotifyAppRemote mSpotifyAppRemote;
+
+
+    private RecyclerView mExPlaylistRecyclerView;
+    private ExPlaylistRecyclerAdapter mExPlaylistAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mExPlaylistRecyclerView = findViewById(R.id.playlistRecyclerView);
+
+        mExPlaylistAdapter = new ExPlaylistRecyclerAdapter(this, new ArrayList< Playlist >());
+        mExPlaylistRecyclerView.setAdapter(mExPlaylistAdapter);
+
+        mExPlaylistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mExPlaylistAdapter.addActionCallback(new ExPlaylistRecyclerAdapter.ActionCallback() {
+            @Override
+            public void onClickListener(Playlist playlist) {
+                //Toast.makeText(MainActivity.this, playlist.getId(), Toast.LENGTH_LONG).show();
+                mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:3cejj3mmTgiLrvNCr5qz83");
+
+                mSpotifyAppRemote.getPlayerApi()
+                        .subscribeToPlayerState()
+                        .setEventCallback(playerState -> {
+                            final Track track = playerState.track;
+                            if (track != null) {
+                                Log.d("MainActivity", track.name + " by " + track.artist.name);
+                            }
+                        });
+
+
+            }
+
+        });
     }
 
     @Override
     protected  void onStart() {
         super.onStart();
+
         ConnectionParams connectionParams =
                 new ConnectionParams.Builder(CLIENT_ID)
                         .setRedirectUri(REDIRECT_URI)
@@ -38,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
             public void onConnected(SpotifyAppRemote spotifyAppRemote) {
                 mSpotifyAppRemote = spotifyAppRemote;
                 Log.d("MainActivity", "Connected! WAHOO WAHOO!");
-                connected();
+                //connected();
             }
 
             @Override
@@ -50,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void connected() {
-        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:3cejj3mmTgiLrvNCr5qz83");
 
         mSpotifyAppRemote.getPlayerApi()
                 .subscribeToPlayerState()
