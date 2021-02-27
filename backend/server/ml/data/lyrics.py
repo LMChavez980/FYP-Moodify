d74750ccd87b0ee80fdbf7d0e3c14711e22a61b3
Louis Chavez <rasengan650@yahoo.com.ph>
2021-02-27 14:55:26 +0000

    Functioning get_songs

--- backend/server/ml/data/lyrics.py
+++ backend/server/ml/data/lyrics.py
@@ -1,10 +1,38 @@
 import lyricsgenius as lg
 from azapi import AZlyrics
-import re
-import csv
 from musixmatch import Musixmatch
+from requests.exceptions import HTTPError, Timeout
 
 GENIUS_CLIENT_ID = '18H6V8QvCrj0kKOefOVWUkkIowkMI84kEv8mK9N01fC_D7ALSIC3PPbbYZoN0vu1'
 GENIUS_CLIENT_SECRET = '187PZ_Um5c3EOZm4rWV8toxjMlzZ84sZfJKhM_D8b4VAhVzXuI1m9TTX49Jrdq7Ysy5s5htzvpCiOAv1RIqQvw'
 GENIUS_CLIENT_ACCESS_TOKEN = 'WdcEvFz0MeZOgDNtrRfAmJ0DReEqv8_MdSwTmQBCBCZCizR5Pf1P7dgfnzKdrOGB'
 MUSIXMATCH_API_KEY = '77a6fdb434976180c2113ea36918734a'
+
+genius = lg.Genius(GENIUS_CLIENT_ACCESS_TOKEN)
+
+genius.remove_section_headers = True
+genius.verbose = False
+genius.retries = 3
+genius.timeout = 8
+
+def get_lyrics(tracks_dict):
+
+    # 1. AZ Lyrics 2. Genius 3. Musixmatch
+    # AZ Lyrics is less picky about titles
+    # Genius is sensitive to titles not matching
+    # Musixmatch will only return 30% of lyrics - approx. up to the first verse
+
+    tracks_dict["lyrics"] = ""
+    tracks_dict["lyric sentiment"] = ""
+
+    try:
+        azlyrics = AZlyrics(search_engine='google')
+
+
+
+        for
+
+
+    except (HTTPError, Timeout) as e:
+
+
--- backend/server/ml/data/song_data.py
+++ backend/server/ml/data/song_data.py
@@ -20,52 +20,81 @@ client_credential_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client
 sp = spotipy.Spotify(client_credentials_manager=client_credential_manager)
 
 
-class Track:
-    def __init__(self, track_name="NA", track_artists="NA", track_id="NA"):
-        self.name = track_name
-        self.artists = track_artists,
-        self.id = track_id
-
-    def get_trackname(self):
-        return self.name
-
-    def get_trackartists(self):
-        return self.artists
-
-    def get_trackid(self):
-        return self.id
-
-    def set_trackname(self, track_name):
-        self.name = track_name
-
-    def set_trackartists(self, track_artists):
-        self.artists = track_artists
-
-    def set_trackid(self, track_id):
-        self.id = track_id
-
-
 def get_tracks(playlist_ids):
-    sp_pl_ids = dict.fromkeys(playlist_ids, None)
+    sp_pl_tracks = []
 
-    for pl_id in sp_pl_ids:
+    for pl_id in playlist_ids:
         results = sp.playlist_items(playlist_id=pl_id, additional_types=['track'])
-        sp_pl_ids[pl_id] = []
         tracks = results['items']
-        sp_pl_ids[pl_id].extend(tracks)
+        sp_pl_tracks.extend(tracks)
         while results['next']:
             results = sp.next(results)
-            sp_pl_ids[pl_id].extend(tracks)
+            tracks = results['items']
+            sp_pl_tracks.extend(tracks)
+
+    sp_track_meta = dict.fromkeys(["id", "name", "artists"], "")
+    id_list = []
+    name_list = []
+    artist_list = []
+
+    for item in sp_pl_tracks:
+        id = item['track']['id']
+        name = item['track']['name']
+        artist = item['track']['artists'][0]['name']
+        id_list.append(id)
+        name_list.append(name)
+        artist_list.append(artist)
+
+    sp_track_meta["id"] = id_list
+    sp_track_meta["name"] = name_list
+    sp_track_meta["artists"] = artist_list
+
+    return sp_track_meta
 
 
+def get_audio_data(sp_tracks):
+    audio_ft = ["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness",
+                "liveness", "valence", "tempo"]
+
+    for ft in audio_ft:
+        sp_tracks[ft] = ""
+
+    danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo = \
+        [], [], [], [], [], [], [], [], [], [], []
+
+    for track_id in sp_tracks['id']:
+        result = sp.audio_features(track_id)
+        danceability.append(result[0]['danceability'])
+        energy.append(result[0]['energy'])
+        key.append(result[0]['key'])
+        loudness.append(result[0]['loudness'])
+        mode.append(result[0]['mode'])
+        speechiness.append(result[0]['speechiness'])
+        acousticness.append(result[0]['acousticness'])
+        instrumentalness.append(result[0]['instrumentalness'])
+        liveness.append(result[0]['liveness'])
+        valence.append(result[0]['valence'])
+        tempo.append(result[0]['tempo'])
+
+    sp_tracks['danceability'] = danceability
+    sp_tracks['energy'] = energy
+    sp_tracks['key'] = key
+    sp_tracks['loudness'] = loudness
+    sp_tracks['mode'] = mode
+    sp_tracks['speechiness'] = speechiness
+    sp_tracks['acousticness'] = acousticness
+    sp_tracks['instrumentalness'] = instrumentalness
+    sp_tracks['liveness'] = liveness
+    sp_tracks['valence'] = valence
+    sp_tracks['tempo'] = tempo
+
+    return sp_tracks
 
 
-
-
-    return sp_pl_ids
-sp_playlists = ["58gtWgHQ99PHtAkCK2dpYt", "3cejj3mmTgiLrvNCr5qz83", "4yMwcuj91F6OJBvK86hMHu"]
+#sp_playlists = ["58gtWgHQ99PHtAkCK2dpYt", "3cejj3mmTgiLrvNCr5qz83", "4yMwcuj91F6OJBvK86hMHu"]
+#sp_playlists = ["58gtWgHQ99PHtAkCK2dpYt"]
 spotify_pl_tks = get_tracks(sp_playlists)
-print(spotify_pl_tks)
+spotify_pl_tks = get_audio_data(spotify_pl_tks)
 
-
-
+#for key in spotify_pl_tks.keys():
+#    print(spotify_pl_tks[key], "\n", len(spotify_pl_tks[key]))
