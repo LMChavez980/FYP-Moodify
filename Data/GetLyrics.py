d38867f4dc7463a13a628819ddb3f6c79bc41b06
Louis Chavez <rasengan650@yahoo.com.ph>
2021-02-27 20:44:10 +0000

    Working on get_lyrics

--- Data/GetLyrics.py
+++ Data/GetLyrics.py
@@ -1,6 +1,7 @@
 import lyricsgenius as lg
 from azapi import AZlyrics
 import re
+from fuzzywuzzy import fuzz
 import csv
 from musixmatch import Musixmatch
 
@@ -11,13 +12,6 @@ def get_lyrics(playlist_dict):
     for playlist_id in playlist_songs.keys():
         for song in playlist_songs[playlist_id]:
             print(0)
-
-
-
-
-
-
-
 
 GENIUS_CLIENT_ID = '18H6V8QvCrj0kKOefOVWUkkIowkMI84kEv8mK9N01fC_D7ALSIC3PPbbYZoN0vu1'
 GENIUS_CLIENT_SECRET = '187PZ_Um5c3EOZm4rWV8toxjMlzZ84sZfJKhM_D8b4VAhVzXuI1m9TTX49Jrdq7Ysy5s5htzvpCiOAv1RIqQvw'
@@ -30,25 +24,41 @@ genius.remove_section_headers = True
 genius.retries = 3
 genius.timeout = 10
 
+song = genius.search_song(title="I Luv Ya", artist="Young Jeezy & Busta Rhymes")
+
+if song is not None:
+    print(fuzz.ratio("Young Jeezy & Busta Rhymes", song.artist))
+    print(song.artist)
+    print(fuzz.ratio("I Luv Ya", song.title))
+    print(song.title)
+    #print(song.lyrics)
+else:
+    print("No song")
+
 google = 'google'
 duckgo = 'duckduckgo'
 
 azlyrics = AZlyrics(search_engine=duckgo)
 
-azlyrics.title = "I don't want to watch the world end with someone else"
-azlyrics.artist = "Clinton Kane"
+azlyrics.title = "Wu-Tang Clan Ain't Nuthing Ta F' Wit"
+azlyrics.artist = "Wu-Tang Clan"
 
 lyrics_az = azlyrics.getLyrics(save=False)
 
 #print(lyrics_az)
 
 #print(lyrics_az)
+regex = re.compile("[\r\t]")
+regex_headers = re.compile("[\[].*?[\]]")
+lyrics_az = re.sub(regex, "", lyrics_az)
+lyrics_az = re.sub(regex_headers, "", lyrics_az)
 
-lyrics_list = lyrics_az.split('\n')
+lyrics = lyrics_az.replace("\n", "\\n")
+#lyrics_list = lyrics_az.split('\n')
 
 #print(lyrics_list)
 
-lyrics = '\\n'.join(lyrics_list)
+#lyrics = '\\n'.join(lyrics_list).strip()
 
 print(lyrics)
 
--- backend/server/ml/data/lyrics.py
+++ backend/server/ml/data/lyrics.py
@@ -1,6 +1,7 @@
 import lyricsgenius as lg
 from azapi import AZlyrics
 from musixmatch import Musixmatch
+import re
 from requests.exceptions import HTTPError, Timeout
 
 GENIUS_CLIENT_ID = '18H6V8QvCrj0kKOefOVWUkkIowkMI84kEv8mK9N01fC_D7ALSIC3PPbbYZoN0vu1'
@@ -28,9 +29,38 @@ def get_lyrics(tracks_dict):
     try:
         azlyrics = AZlyrics(search_engine='google')
 
+        for i in range(len(tracks_dict['id'])):
+            track_name = tracks_dict['name'][i]
+            track_artist = tracks_dict['artists'][i]
+            azlyrics.title = track_name
+            azlyrics.artist = track_artist
+
+            res_lyrics = azlyrics.getLyrics()
+
+            # If there is a valid result
+            if res_lyrics != 0:
+                regex = re.compile("[\r\t]")
+                regex_headers = re.compile("[\[].*?[\]]")
+                res_lyrics = re.sub(regex, "", res_lyrics)
+                res_lyrics = re.sub(regex_headers, "", res_lyrics)
+                lyrics = res_lyrics.replace("\n", "\\n")
+                tracks_dict["lyrics"] = lyrics
+            else:
+                song = genius.search_song(title=track_name, artist=track_artist)
+
+                if song is not None:
+                    artist = song.artist.replace("’", "'").lower()
+                    title = song.title.replace("’", "'").lower()
 
 
-        for
+
+
+
+
+
+
+
+
 
 
     except (HTTPError, Timeout) as e:
