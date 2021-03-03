da576f653af9a7c433a773c907047c9c3ca9d378
Louis Chavez <rasengan650@yahoo.com.ph>
2021-03-03 21:46:50 +0000

    Tweaked lyrics function: separated engines to functions, now using lyrisgenius 2.02 from 3.0

--- Data/GetSongs.py
+++ Data/GetSongs.py
@@ -1,5 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import lyricsgenius as lg
 
 
 CLIENT_ID = "51891ef3095849e98336d9d0c83c05e9"
@@ -8,16 +9,38 @@ CLIENT_SECRET = "d833852dd80b4117b0e020e480f142c6"
 client_credential_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credential_manager)
 
-result = sp.playlist_items(playlist_id="4b1Ea2rqiJl57ZKC2IPymd", additional_types=['track'], market='IE')
+result = sp.playlist_items(playlist_id="58gtWgHQ99PHtAkCK2dpYt", additional_types=['track'], market='IE')
 tracks = result['items']
-print("Start", len(tracks))
+
 while result['next']:
-    print(result['next'])
     result = sp.next(result)
     tracks.extend(result['items'])
-    print("New:", len(tracks))
 
-print(len(tracks))
+print(tracks)
 
+tracks_dict = dict.fromkeys(["title", "artist"], "")
 
+titles = []
+artists = []
+
+for item in tracks:
+    titles.append(item['track']['name'])
+    artists.append(item['track']['artists'][0]['name'])
+
+tracks_dict["title"] = titles
+tracks_dict["artist"] = artists
+
+print(tracks_dict["title"])
+print(tracks_dict["artist"])
+
+GENIUS_CLIENT_ACCESS_TOKEN = 'WdcEvFz0MeZOgDNtrRfAmJ0DReEqv8_MdSwTmQBCBCZCizR5Pf1P7dgfnzKdrOGB'
+genius = lg.Genius(GENIUS_CLIENT_ACCESS_TOKEN)
+genius.remove_section_headers = True
+genius.retries = 3
+genius.timeout = 10
+
+for i in range(len(tracks_dict["title"])):
+    song = genius.search_song(title=tracks_dict["title"][i], artist=tracks_dict["artist"][i])
+    print("Title:", song.title)
+    print("Artist:", song.artist, "\n")
 
--- backend/server/ml/data/lyrics.py
+++ backend/server/ml/data/lyrics.py
@@ -5,7 +5,6 @@ import re
 from requests.exceptions import HTTPError, Timeout
 from fuzzywuzzy import fuzz
 
-
 GENIUS_CLIENT_ID = '18H6V8QvCrj0kKOefOVWUkkIowkMI84kEv8mK9N01fC_D7ALSIC3PPbbYZoN0vu1'
 GENIUS_CLIENT_SECRET = '187PZ_Um5c3EOZm4rWV8toxjMlzZ84sZfJKhM_D8b4VAhVzXuI1m9TTX49Jrdq7Ysy5s5htzvpCiOAv1RIqQvw'
 GENIUS_CLIENT_ACCESS_TOKEN = 'WdcEvFz0MeZOgDNtrRfAmJ0DReEqv8_MdSwTmQBCBCZCizR5Pf1P7dgfnzKdrOGB'
@@ -20,66 +19,99 @@ genius.timeout = 8
 
 musixmatch = Musixmatch(MUSIXMATCH_API_KEY)
 
+
+def genius_lyrics(song_title, song_artist):
+    print("Genius")
+    try:
+        song = genius.search_song(title=song_title, artist=song_artist)
+
+        if song is not None:
+            artist = song.artist.replace("’", "'").lower()
+            title = song.title.replace("’", "'").lower()
+            if fuzz.partial_ratio(song_artist.lower(), artist) > 70 and fuzz.partial_ratio(song_title.lower(), title) > 70:
+                lyrics = song.lyrics.replace("\n", "\\n")
+                print("Found Lyrics!")
+                return lyrics
+            else:
+                return None
+
+        return None
+
+    except (HTTPError, Timeout) as e:
+        return None
+
+
+def az_lyrics(song_title, song_artist):
+    print("AZ Lyrics")
+    azlyrics = AZlyrics(search_engine='duckduckgo')
+    azlyrics.title = song_title
+    azlyrics.artist = song_artist
+
+    lyrics = azlyrics.getLyrics(save=False)
+
+    if lyrics != 0:
+        regex = re.compile("[\r\t]")
+        regex_headers = re.compile("[\[].*?[\]]")
+        lyrics = re.sub(regex, "", lyrics)
+        lyrics = re.sub(regex_headers, "", lyrics)
+        lyrics = lyrics.replace("\n", "\\n")
+        print("Found Lyrics!")
+        return lyrics
+    else:
+        return None
+
+
+def musixmatch_lyrics(song_title, song_artist):
+    print("Musicxmatch")
+    match = musixmatch.matcher_lyrics_get(q_track=song_title, q_artist=song_artist)
+    if match['message']['header']['status_code'] == 200:
+        tail = 75
+        head = len(match['message']['body']['lyrics']['lyrics_body'])
+        lyrics = match['message']['body']['lyrics']['lyrics_body'][:head - tail]
+        print("Found Lyrics!")
+        return lyrics
+    else:
+        return None
+
+
 def get_lyrics(tracks_dict):
 
-    # 1. AZ Lyrics 2. Genius 3. Musixmatch
-    # AZ Lyrics is less picky about titles
+    # 1. Genius 2. AZ Lyrics 3. Musixmatch
+    # AZ Lyrics is less picky about titles however can be blocked by browser
     # Genius is sensitive to titles not matching
     # Musixmatch will only return 30% of lyrics - approx. up to the first verse
 
     tracks_dict["lyrics"] = ""
     lyrics_list = []
-    lyrics_unavailable = []
 
     try:
-        method = 1
-        azlyrics = AZlyrics(search_engine='duckduckgo')
-
         for i in range(len(tracks_dict['id'])):
             track_name = tracks_dict['name'][i]
             track_artist = tracks_dict['artists'][i]
             print("Search:", track_name, "-", track_artist)
-            azlyrics.title = track_name
-            azlyrics.artist = track_artist
 
-            res_lyrics = azlyrics.getLyrics(save=False)
+            # Genius
+            lyrics = genius_lyrics(track_name, track_artist)
 
-            # If there is a valid result
-            if res_lyrics != 0:
-                print("AZ Lyrics")
-                regex = re.compile("[\r\t]")
-                regex_headers = re.compile("[\[].*?[\]]")
-                res_lyrics = re.sub(regex, "", res_lyrics)
-                res_lyrics = re.sub(regex_headers, "", res_lyrics)
-                lyrics = res_lyrics.replace("\n", "\\n")
+            # if Genius returns with lyrics - add to list
+            # else call AZ Lyrics - call Musixmatch if that fails
+            # Add empty string if none found
+            if lyrics is not None:
                 lyrics_list.append(lyrics)
             else:
-                method = 2
-                song = genius.search_song(title=track_name, artist=track_artist)
+                # AZ Lyrics
+                lyrics = az_lyrics(track_name, track_artist)
 
-                if song is not None:
-                    print("Genius Lyrics")
-                    artist = song.artist.replace("’", "'").lower()
-                    title = song.title.replace("’", "'").lower()
-                    print(title, "-",artist)
-                    if fuzz.partial_ratio(track_artist.lower(), artist) > 70 and fuzz.partial_ratio(track_name.lower(), title) > 70:
-                        lyrics = song.lyrics.replace("\n", "\\n")
+                if lyrics is not None:
+                    lyrics_list.append(lyrics)
+                else:
+                    # Musixmatch
+                    lyrics = musixmatch_lyrics(track_name, track_artist)
+
+                    if lyrics is not None:
                         lyrics_list.append(lyrics)
                     else:
-                        method = 3
-                        print("Musixmatch Lyrics")
-                        ret = lyrics_musixmatch(track_name, track_artist)
-                        if ret != "":
-                            lyrics_list.append(ret)
-                        else:
-                            lyrics_list.append("")
-                else:
-                    method = 3
-                    print("Musixmatch Lyrics")
-                    ret = lyrics_musixmatch(track_name, track_artist)
-                    if ret != "":
-                        lyrics_list.append(ret)
-                    else:
+                        print("Could not find song")
                         lyrics_list.append("")
 
     except (HTTPError, Timeout) as e:
@@ -91,15 +123,3 @@ def get_lyrics(tracks_dict):
 
     return tracks_dict
 
-
-def lyrics_musixmatch(title, artist):
-    match = musixmatch.matcher_lyrics_get(q_track=title, q_artist=artist)
-    if match['message']['header']['status_code'] == 200:
-        tail = 75
-        head = len(match['message']['body']['lyrics']['lyrics_body'])
-        lyrics = match['message']['body']['lyrics']['lyrics_body'][:head - tail]
-
-        return lyrics
-    else:
-        print("Couldn't find song")
-        return ""
