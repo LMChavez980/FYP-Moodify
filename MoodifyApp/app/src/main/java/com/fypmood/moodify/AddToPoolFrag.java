d41c096feb431c7911a0a8980d46c3f7fa08241a
Louis Chavez <rasengan650@yahoo.com.ph>
2021-03-19 02:05:21 +0000

    Transfer functionality to Fragments - bottom nav bar

---
+++ MoodifyApp/app/src/main/java/com/fypmood/moodify/AddToPoolFrag.java
@@ -0,0 +1,91 @@
+package com.fypmood.moodify;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.PlaylistSimple;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class AddToPoolFrag extends Fragment {
+
+    private ExPlaylistRecyclerAdapter mExPlaylistAdapter;
+    private SpotifyApi api = new SpotifyApi();
+
+    public AddToPoolFrag(){}
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState){
+
+        View rootView = inflater.inflate(R.layout.fragment_pool, container, false);
+
+        MainActivity mainActivity = (MainActivity) getActivity();
+
+        String auth_token = mainActivity.mSharedPreferences.getString("TOKEN", "");
+
+        RecyclerView mExPlaylistRecyclerView = rootView.findViewById(R.id.playlistRecyclerView);
+        mExPlaylistRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        api.setAccessToken(auth_token);
+        SpotifyService spotify = api.getService();
+
+        spotify.getMyPlaylists(new Callback<Pager<PlaylistSimple>>() {
+            @Override
+            public void success(Pager<PlaylistSimple> playlistSimplePager, Response response) {
+                List<PlaylistSimple> myPlaylists = playlistSimplePager.items;
+                Log.i("ATP", "My playlists allocated");
+                mExPlaylistAdapter = new ExPlaylistRecyclerAdapter(getContext(), myPlaylists);
+                mExPlaylistRecyclerView.setAdapter(mExPlaylistAdapter);
+
+                FloatingActionButton analyzeFab = rootView.findViewById(R.id.sendToAnalyze);
+                analyzeFab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ArrayList<String> mPlaylistsIds = new ArrayList<String>();
+                        int len = mExPlaylistAdapter.getItemCount();
+                        List<PlaylistSimple> mPlaylists = mExPlaylistAdapter.getmPlaylistList();
+                        SparseBooleanArray chosen = mExPlaylistAdapter.getCheckBoxState();
+                        String id = "";
+                        for(int i = 1; i < len; i++)
+                        {
+                            if(chosen.get(i))
+                            {
+                                id = mPlaylists.get(i).id;
+                                mPlaylistsIds.add(id);
+                            }
+                        }
+                        Toast.makeText(getContext(), mPlaylistsIds.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e("ATP", error.getMessage());
+            }
+        });
+
+        return rootView;
+    }
+
+}
--- MoodifyApp/app/src/main/java/com/fypmood/moodify/MainActivity.java
+++ MoodifyApp/app/src/main/java/com/fypmood/moodify/MainActivity.java
@@ -4,20 +4,30 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CLIENT_ID = "4110566732ad4c08b6f0e6c5768e552d";
+    private static final String REDIRECT_URI = "com.fypmood.moodify://callback";
+    private static final int REQUEST_CODE = 1337;
+    private SpotifyAppRemote mSpotifyAppRemote;
     SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         mSharedPreferences = getSharedPreferences("SPOTIFY", 0);
@@ -28,7 +38,14 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MAIN", "Token: ".concat(auth_token));
         Log.i("MAIN", "User ID: ".concat(id));
 
-        Button toGen = findViewById(R.id.to_generate);
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // Set Default fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new GenerateFrag()).commit();
+
+        /*Button toGen = findViewById(R.id.to_generate);
         Button toPool = findViewById(R.id.to_pool);
         Button toStats = findViewById(R.id.to_stats);
 
@@ -53,9 +70,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
             }
-        });
+        });*/
 
 
     }
 
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            menuItem -> {
+                Fragment selectedFragment = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_generate:
+                        selectedFragment = new GenerateFrag();
+                        getSupportActionBar().setTitle(R.string.select_mood_btn);
+                        break;
+
+                    case R.id.nav_add_to_pool:
+                        selectedFragment = new AddToPoolFrag();
+                        getSupportActionBar().setTitle(R.string.add_to_pool_btn);
+                        break;
+
+                    case R.id.nav_statistics:
+                        selectedFragment = new StatisticsFrag();
+                        getSupportActionBar().setTitle(R.string.mood_stats_btn);
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            };
 }
