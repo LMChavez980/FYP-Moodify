4b66131073e4a8768b93e5e359a29ad4cb662db6
Louis Chavez <rasengan650@yahoo.com.ph>
2021-03-19 18:44:49 +0000

    added alternate for generate playlist

--- MoodifyApp/app/src/main/java/com/fypmood/moodify/GenerateFrag.java
+++ MoodifyApp/app/src/main/java/com/fypmood/moodify/GenerateFrag.java
@@ -12,7 +12,7 @@ public class GenerateFrag extends Fragment {
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_generate, container, false);
+        return inflater.inflate(R.layout.fragment_generate_grid, container, false);
     }
 
 }
